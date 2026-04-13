0612f998949f5064b9b41090aff879a8be3b4a77
Marins <jmarinsti@gmail.com>
2026-04-13 09:36:52 -0300

    Bean Validation

--- src/main/java/com/devsuperior/crud/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/devsuperior/crud/controllers/handlers/ControllerExceptionHandler.java
@@ -1,11 +1,13 @@
 package com.devsuperior.crud.controllers.handlers;
 
 import com.devsuperior.crud.dto.CustomError;
+import com.devsuperior.crud.dto.ValidationError;
 import com.devsuperior.crud.services.exceptions.DatabaseException;
 import com.devsuperior.crud.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -33,7 +35,10 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<CustomError> methodArgument(MethodArgumentNotValidException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY ;
-        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        ValidationError err = new ValidationError(Instant.now(), status.value(), "Dados inválidos", request.getRequestURI());
+        for (FieldError f : e.getBindingResult().getFieldErrors()) {
+            err.addError(f.getField(), f.getDefaultMessage());
+        }
         return ResponseEntity.status(status).body(err);
     }
 }
